b8c68d8c910d3b1b6c84188396feb878327cdf4e
Eskandar Atrakchi <102361045+EskandarAtrakchi@users.noreply.github.com>
2023-10-02 15:28:04 +0100

    02/Oct/2023

---
+++ Advanced Databases/Algos/LinearSearch.java
@@ -0,0 +1,19 @@
+public class LinearSearch {
+
+    public static void main (String [] args) {
+
+        int [] data = {10, 9, 101, 50, 99, 66, 12, -5};
+
+        for (int indexNum = 0; indexNum < data.length; indexNum++) {
+
+            if (data[indexNum] == query) {
+                System.out.println("Found at position: " + indexNum) {
+                    indexNum = data.length;//stopping the loop 
+                }
+            }
+
+        }
+        
+    }
+
+}
