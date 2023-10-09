6ae1d7feda0ff1a9d4c5b56aa625e64fb74db912
Eskandar Atrakchi <102361045+EskandarAtrakchi@users.noreply.github.com>
2023-10-09 15:05:55 +0100

    week3

--- Advanced Databases/Algos/BinarySearch.java
+++ Advanced Databases/Algos/BinarySearch.java
@@ -2,53 +2,53 @@ import java.util.Scanner;
 
 public class BinarySearch {
     public static void main(String args[]){
-     int [] data = {10, 9, 101, 50, 99, 66, 12, -5};
+     int [] data = {10, 9, 101, 50, 99, 66, 12, -5};// 
 
         Scanner sc = new Scanner(System.in);
-        int query = sc.nextInt();
+        int query = sc.nextInt();// taking input from user
 
-        sortData(data);
+        sortData(data);// sorting the data
         //printData(data);
         
 
-        int lowerIndex = 0;
-        int upperIndex = data.length-1;
-        boolean found = false;
+        int lowerIndex = 0;// lower index
+        int upperIndex = data.length-1;// upper index
+        boolean found = false;// flag to check if data is found
 
-        while(!found && upperIndex >= lowerIndex){
-            int midPoint = ((upperIndex - lowerIndex)/2) + lowerIndex;
+        while(!found && upperIndex >= lowerIndex){// while loop to check if data is found
+            int midPoint = ((upperIndex - lowerIndex)/2) + lowerIndex;// calculating the mid point
             
-            if(data[midPoint] == query){
-                found = true;
+            if(data[midPoint] == query){// checking if the data is found
+                found = true;// flag to check if data is found
                 System.out.println("Data is found at: " + midPoint);
             }
-            else if(data[midPoint] > query){
-                upperIndex = midPoint-1;
+            else if(data[midPoint] > query){// checking if the data is greater than the query
+                upperIndex = midPoint-1;// decrementing the upper index
             }
             else{
-                lowerIndex = midPoint+1;
+                lowerIndex = midPoint+1;// decrementing the lower index
             }
         }
         
-        if(!found){
+        if(!found){// if the data is not found
             System.out.println("Data is not in the array");
         }
     }
 
-    static void printData(int data[]){
-        for(int d : data){
-            System.out.print(d + ", ");
+    static void printData(int data[]){// printing the data
+        for(int d : data){// printing the data
+            System.out.print(d + ", ");// printing the data
         }
     }
 
-    static void sortData(int data[]){
+    static void sortData(int data[]){// sorting the data
         // sorting : bubblesort
-        for(int i=0; i<data.length; i++){
-            for(int j=0; j<data.length; j++){
-                if(data[i] < data[j]){
-                    int temp= data[i];
-                    data[i] = data[j];
-                    data[j] = temp;
+        for(int i=0; i<data.length; i++){// for loop to sort the data
+            for(int j=0; j<data.length; j++){// for loop to sort the data
+                if(data[i] < data[j]){// checking if the data is greater than the query
+                    int temp= data[i];// assigning the data to temp
+                    data[i] = data[j];// assigning the data to the query
+                    data[j] = temp;// assigning the data to the query
                 }
             }
         }
--- Advanced Databases/Algos/LinearSearch.java
+++ Advanced Databases/Algos/LinearSearch.java
@@ -1,27 +1,27 @@
 import java.util.Scanner;
 
-import javax.swing.JOptionPane;
+import javax.swing.JOptionPane;// import the JOptionPane class
 public class LinearSearch {
     public static void main(String args[]){
         int [] data = new int[100];// random value array
-        for(int i=0; i<data.length; i++){
-            data[i] = (int)(Math.random() * 1000); 
+        for(int i=0; i<data.length; i++){// for loop to generate random values
+            data[i] = (int)(Math.random() * 1000); // generate random value
         }
 
         int query = Integer.parseInt(JOptionPane.showInputDialog(null, "Enter a value to search for within the array"));
         
-        data[76] = query; 
+        data[76] = query; // set the value to be searched
 
-        int counter = 0;
-        for(int indexNum = 0; indexNum < data.length; indexNum++){
-            if(data[indexNum] == query){
+        int counter = 0;// count the number of times the value was found
+        for(int indexNum = 0; indexNum < data.length; indexNum++){// for loop to search for the value
+            if(data[indexNum] == query){// if the value was found, print the position
                 System.out.println("Found at position: " + indexNum);
-                indexNum = data.length;
+                indexNum = data.length;// reset the indexNum to the end of the array
             }
-            counter++;
+            counter++;// increment the counter
         }
 
-        System.out.println("This took " + counter + " many steps");
+        System.out.println("This took " + counter + " many steps");// print the number of times the
 
     }
 }
