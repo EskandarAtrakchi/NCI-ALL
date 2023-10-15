5e90d88466f5fe75d47b4b61c222dc279602d780
Eskandar Atrakchi <102361045+EskandarAtrakchi@users.noreply.github.com>
2023-10-15 21:39:06 +0100

    week4

---
+++ OOP/BookingsWeek4/Bookings/BookingApp.java
@@ -0,0 +1,82 @@
+
+package bookingapp;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author escan
+ */
+public class BookingApp {
+
+    static Scanner input = new Scanner(System.in);
+    
+    public static void main(String[] args) {
+        
+        //instance for the bookings class will be used if it is needed 
+        
+        Bookings booking = new Bookings ();
+        
+        //instance for the CateringCottages class
+        
+        CateringCottages cc = new CateringCottages();
+        
+        //instance for the HotelRomms class
+        
+        HotelRooms hr = new HotelRooms ();
+        
+        //setting the variable in CateringCottages class 
+        
+        System.out.println("Please choose number one for the CateringCottages and number two for HotelRooms:");
+        int X = input.nextInt();
+        int Y = 1;
+        while (Y == 1) {
+            
+            if (X == 1) {
+
+                cc.setAddress("Camden Street Upper");
+                cc.setKeyCollectionPoint('D');
+
+                //getting them via getters 
+
+                System.out.println("this is the address of the hotel: " + cc.getAddress());
+                System.out.println("This is the collection point: " + cc.getKeyCollectionPoint());
+            
+                break;
+                
+            }
+            
+            else if (X == 2) {
+               
+                ////setting the variable in HotelRooms class 
+
+                hr.setBreakFast(true);
+                hr.setRoomNumber("4");
+
+                //getting them via getters 
+
+                if (hr.isBreakFast() == true) {
+                    String BreakfastIncluded = "The breakfast is included.";
+                    System.out.println(BreakfastIncluded);
+                }
+                else if (hr.isBreakFast() == false){
+                    String BreakfastIsNotIncluded = "The breakfast is not included.";
+                    System.out.println(BreakfastIsNotIncluded);
+                }
+
+                System.out.println("The room number is: " + hr.getRoomNumber());
+                
+                break;
+                
+            }
+            
+            else {
+                
+                System.out.println("Opps,Wrong choice!");
+                break;
+                
+            }
+        }
+    }
+    
+}
---
+++ OOP/BookingsWeek4/Bookings/CateringCottages.java
@@ -0,0 +1,33 @@
+
+package bookingapp;
+
+/**
+ *
+ * @author escan
+ */
+public class CateringCottages {
+    
+    private String address;
+    private char keyCollectionPoint;
+
+    public CateringCottages() {
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public char getKeyCollectionPoint() {
+        return keyCollectionPoint;
+    }
+
+    public void setKeyCollectionPoint(char keyCollectionPoint) {
+        this.keyCollectionPoint = keyCollectionPoint;
+    }
+    
+    
+}
