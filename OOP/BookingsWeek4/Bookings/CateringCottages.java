b6a2a154364ff042dc66c482da0f975ead8d6641
Eskandar Atrakchi <102361045+EskandarAtrakchi@users.noreply.github.com>
2023-10-15 21:41:39 +0100

    week4

--- OOP/BookingsWeek4/Bookings/CateringCottages.java
+++ OOP/BookingsWeek4/Bookings/CateringCottages.java
@@ -1,18 +1,44 @@
 
 package bookingapp;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author escan
  */
-public class CateringCottages {
+public class CateringCottages extends Bookings {
     
     private String address;
     private char keyCollectionPoint;
+    private double cottageCost;
+    
+    //we will need this array later in BookingApp class 
+    static ArrayList<CateringCottages> cottageList = new ArrayList<CateringCottages>();
 
     public CateringCottages() {
     }
 
+    //we will need this constructor in BookingApp class 
+    //Note, be careful follow the same sequance of the parameter in the constructor 
+    
+    public CateringCottages(String address, char keyCollectionPoint, double cottageCost, String clientName, String bookingReference, String startDate, double nightDuration) {
+        
+        super(clientName, bookingReference, startDate, nightDuration);
+        this.address = address;
+        this.keyCollectionPoint = keyCollectionPoint;
+        this.cottageCost = cottageCost;
+        
+    }
+    
+    public double getCottageCost() {
+        return cottageCost;
+    }
+
+    public void setCottageCost(double cottageCost) {
+        this.cottageCost = cottageCost;
+    }
+    
     public String getAddress() {
         return address;
     }
--- OOP/BookingsWeek4/Bookings/HotelRooms.java
+++ OOP/BookingsWeek4/Bookings/HotelRooms.java
@@ -1,18 +1,43 @@
 
 package bookingapp;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author escan
  */
-public class HotelRooms {
+public class HotelRooms extends Bookings {
     
     private String roomNumber;
     private boolean breakFast;
+    private double hotelRoomCost;
+    
+    //we will need this array later in BookingApp class 
+    static ArrayList<HotelRooms> roomsList = new ArrayList<HotelRooms>();
 
     public HotelRooms() {
     }
 
+    //we will need this constructor in BookingApp class 
+    //Note, be careful follow the same sequance of the parameter in the constructor 
+    public HotelRooms(String roomNumber, boolean breakFast, double hotelRoomCost, String clientName, String bookingReference, String startDate, double nightDuration) {
+        
+        super(clientName, bookingReference, startDate, nightDuration);
+        this.roomNumber = roomNumber;
+        this.breakFast = breakFast;
+        this.hotelRoomCost = hotelRoomCost;
+        
+    }
+
+    public double getHotelRoomCost() {
+        return hotelRoomCost;
+    }
+
+    public void setHotelRoomCost(double hotelRoomCost) {
+        this.hotelRoomCost = hotelRoomCost;
+    }
+    
     public String getRoomNumber() {
         return roomNumber;
     }
