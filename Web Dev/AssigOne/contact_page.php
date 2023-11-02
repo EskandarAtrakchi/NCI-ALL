8b6564e4ad99f79d61eb09e47069db8751e05a28
Eskandar Atrakchi <102361045+EskandarAtrakchi@users.noreply.github.com>
2023-11-02 00:31:22 +0000

    .

---
+++ Web Dev/AssigOne/contact_page.php
@@ -0,0 +1,16 @@
+<?php
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $to = "example@gmail.com";
+            $subject = "Contact Form Submission";
+
+            $message = "First Name: " . $_POST["fname"] . "\n";
+            $message .= "Last Name: " . $_POST["surname"] . "\n";
+            $message .= "Contacting Us For: " . $_POST["contactus"] . "\n";
+            $message .= "Subject: " . $_POST["subject"] . "\n";
+
+            $headers = "From: " . $_POST["fname"] . " " . $_POST["surname"] . "\r\n";
+
+            mail($to, $subject, $message, $headers);
+        }
+    ?>
+    
