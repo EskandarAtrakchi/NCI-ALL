5840563b6f78e5393f0c8de47f10f58c7c8314cf
Eskandar Atrakchi <102361045+EskandarAtrakchi@users.noreply.github.com>
2023-11-04 18:29:49 +0000

    .

--- Advanced Databases/AssigOne/quesries.sql
+++ Advanced Databases/AssigOne/quesries.sql
@@ -1,92 +1,75 @@
-Certainly, here are ten example queries that you can run on the provided tables:
+-----------------------------------------------------------------------------------
+--  1. Retrieve the names and ages of all students:
+-----------------------------------------------------------------------------------
 
---**Query 1:** Retrieve a list of students who attended Module A in January 2023, including their names, attendance status, and performance scores.
+SELECT StudentName, StudentAge FROM StudentDim;
 
-SELECT s.StudentName, f.AttendanceStatus, f.PerformanceScore
-FROM TrainingAttendanceFact f
-JOIN StudentDim s ON f.StudentKey = s.StudentKey
-JOIN ModuleDim m ON f.ModuleKey = m.ModuleKey
-JOIN TimeDim t ON f.TimeKey = t.TimeKey
-WHERE m.ModuleName = 'Module A' AND t.Month = 'January' AND t.Year = 2023;
+-----------------------------------------------------------------------------------
+--  2. List all modules and their descriptions:
+-----------------------------------------------------------------------------------
 
+SELECT ModuleName, ModuleDescription FROM ModuleDim;
 
---**Query 2:** Calculate the average performance score for Module B across all time periods.
+-----------------------------------------------------------------------------------
+--  3. Find the instructors for each module:
+-----------------------------------------------------------------------------------
 
-SELECT m.ModuleName, AVG(f.PerformanceScore) AS AveragePerformanceScore
-FROM TrainingAttendanceFact f
-JOIN ModuleDim m ON f.ModuleKey = m.ModuleKey
-GROUP BY m.ModuleName
-HAVING m.ModuleName = 'Module B';
+SELECT ModuleName, ModuleInstructor FROM ModuleDim;
 
+-----------------------------------------------------------------------------------
+--  4. Get a list of students who attended training:
+-----------------------------------------------------------------------------------
 
---**Query 3:** List instructors and their email addresses who had the highest attendance rates in the year 2023.
+SELECT StudentName FROM StudentDim
+WHERE StudentKey IN (SELECT StudentKey FROM TrainingAttendanceFact WHERE AttendanceStatus = true);
 
-SELECT i.InstructorName, i.InstructorEmail
-FROM TrainingAttendanceFact f
-JOIN InstructorDim i ON f.InstructorKey = i.InstructorKey
-JOIN TimeDim t ON f.TimeKey = t.TimeKey
-WHERE t.Year = 2023
-GROUP BY i.InstructorName, i.InstructorEmail
-HAVING COUNT(*) = (SELECT MAX(AttendanceCount) FROM (SELECT COUNT(*) AS AttendanceCount FROM TrainingAttendanceFact GROUP BY InstructorKey) AS T);
+-----------------------------------------------------------------------------------
+--  5. Find the total number of students in each location:
+-----------------------------------------------------------------------------------
 
+SELECT LocationName, COUNT(StudentKey) AS TotalStudents
+FROM LocationDim
+JOIN TrainingAttendanceFact ON LocationDim.LocationKey = TrainingAttendanceFact.LocationKey
+GROUP BY LocationName;
 
---**Query 4:** Find the location with the lowest utilization in terms of the number of training sessions held.
+-----------------------------------------------------------------------------------
+--  6. Retrieve the modules with their start and end dates in the first quarter of 2023:
+-----------------------------------------------------------------------------------
 
-SELECT l.LocationName, COUNT(*) AS TrainingSessionCount
-FROM TrainingAttendanceFact f
-JOIN LocationDim l ON f.LocationKey = l.LocationKey
-GROUP BY l.LocationName
-ORDER BY TrainingSessionCount ASC
-LIMIT 1;
+SELECT ModuleName, ModuleStartDate, ModuleEndDate
+FROM ModuleDim
+WHERE ModuleStartDate BETWEEN '2023-01-01' AND '2023-03-31';
 
+-----------------------------------------------------------------------------------
+--  7. Find the average performance score for all students:
+-----------------------------------------------------------------------------------
 
---**Query 5:** Retrieve a list of students who have not attended any training sessions.
+SELECT AVG(PerformanceScore) AS AveragePerformanceScore FROM TrainingAttendanceFact;
 
-SELECT s.StudentName
+-----------------------------------------------------------------------------------
+--  8. List the instructors who have students attending training and their contact information:
+-----------------------------------------------------------------------------------
+
+SELECT DISTINCT i.InstructorName, i.InstructorEmail, i.InstructorPhone
+FROM InstructorDim i
+JOIN TrainingAttendanceFact t ON i.InstructorKey = t.InstructorKey;
+
+-----------------------------------------------------------------------------------
+--  9. Count the number of students who attended training for each module:
+-----------------------------------------------------------------------------------
+
+SELECT ModuleName, COUNT(StudentKey) AS AttendanceCount
+FROM ModuleDim m
+LEFT JOIN TrainingAttendanceFact t ON m.ModuleKey = t.ModuleKey
+WHERE AttendanceStatus = true
+GROUP BY ModuleName;
+
+-----------------------------------------------------------------------------------
+--  10. Retrieve the students who attended training on a specific date and their performance scores:
+-----------------------------------------------------------------------------------
+
+SELECT s.StudentName, t.PerformanceScore
 FROM StudentDim s
-LEFT JOIN TrainingAttendanceFact f ON s.StudentKey = f.StudentKey
-WHERE f.StudentKey IS NULL;
-
-
---**Query 6:** Find the modules with the highest average performance scores.
-
-SELECT m.ModuleName, AVG(f.PerformanceScore) AS AveragePerformanceScore
-FROM TrainingAttendanceFact f
-JOIN ModuleDim m ON f.ModuleKey = m.ModuleKey
-GROUP BY m.ModuleName
-ORDER BY AveragePerformanceScore DESC;
-
-
---**Query 7:** List instructors and their average performance scores.
-
-SELECT i.InstructorName, AVG(f.PerformanceScore) AS AveragePerformanceScore
-FROM TrainingAttendanceFact f
-JOIN InstructorDim i ON f.InstructorKey = i.InstructorKey
-GROUP BY i.InstructorName;
-
-
---**Query 8:** Identify students with the best and worst overall performance scores.
-
-SELECT s.StudentName, MAX(f.PerformanceScore) AS MaxPerformanceScore, MIN(f.PerformanceScore) AS MinPerformanceScore
-FROM TrainingAttendanceFact f
-JOIN StudentDim s ON f.StudentKey = s.StudentKey
-GROUP BY s.StudentName;
-
-
---**Query 9:** Find the most popular training location (the location with the highest attendance).
-
-SELECT l.LocationName, COUNT(*) AS AttendanceCount
-FROM TrainingAttendanceFact f
-JOIN LocationDim l ON f.LocationKey = l.LocationKey
-GROUP BY l.LocationName
-ORDER BY AttendanceCount DESC
-LIMIT 1;
-
-
---**Query 10:** Retrieve a list of modules along with the names of the students who achieved a perfect score (100) in those modules.
-
-SELECT m.ModuleName, s.StudentName
-FROM TrainingAttendanceFact f
-JOIN ModuleDim m ON f.ModuleKey = m.ModuleKey
-JOIN StudentDim s ON f.StudentKey = s.StudentKey
-WHERE f.PerformanceScore = 100;
+JOIN TrainingAttendanceFact t ON s.StudentKey = t.StudentKey
+JOIN TimeDim tm ON t.TimeKey = tm.TimeKey
+WHERE tm.DimDate = '2023-03-10';
