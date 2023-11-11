def21633008c518d3e2b0e94122f1a08b2426081
Eskandar Atrakchi <102361045+EskandarAtrakchi@users.noreply.github.com>
2023-11-11 14:03:13 +0000

    .

--- Advanced Databases/AssigOne/Python-DB/StudentDimConnection.py
+++ Advanced Databases/AssigOne/Python-DB/StudentDimConnection.py
@@ -1,23 +1,54 @@
 import pandas as pd
 import mysql.connector
 
-data_url = 'StudentDim.json'
-df = pd.read_json(data_url)
-print(df)
+time_df = pd.read_json('TimeDim.json')
+institution_df = pd.read_json('InstitutionDim.json')
+module_df = pd.read_json('ModuleDim.json')
+student_df = pd.read_json('StudentDim.json')
 
 # Establish a MySQL connection
 conn = mysql.connector.connect(
     host='127.0.0.1',
     user='root',
     password='0723524754Aa.',
-    database='test'
+    database='Assig'
 )
 
 # Create a cursor object
 cursor = conn.cursor()
 
+# Insert data into TimeDim table with handling duplicates
+for index, row in time_df.iterrows():
+    try:
+        cursor.execute("""
+            INSERT INTO TimeDim (DateID, DimMonth, Dayoff, TrainingDays, DimQuarter, DimYear)
+            VALUES (%s, %s, %s, %s, %s, %s)
+        """, (row['DateID'], row['DimMonth'], row['Dayoff'], row['TrainingDays'], row['DimQuarter'], row['DimYear']))
+    except mysql.connector.IntegrityError as e:
+        print(f"Skipping duplicate entry for DateID {row['DateID']}")
+
+# Insert data into InstitutionDim table with handling duplicates
+for index, row in institution_df.iterrows():
+    try:
+        cursor.execute("""
+            INSERT INTO InstitutionDim (InstitutionID, InstitutionName, AmountOfStuff, OpenTime, CloseTime, Address, InstitutionEmail)
+            VALUES (%s, %s, %s, %s, %s, %s, %s)
+        """, (row['InstitutionID'], row['InstitutionName'], row['AmountOfStuff'], row['OpenTime'], row['CloseTime'], row['Address'], row['InstitutionEmail']))
+    except mysql.connector.IntegrityError as e:
+        print(f"Skipping duplicate entry for InstitutionID {row['InstitutionID']}")
+
+# Insert data into ModuleDim table with handling duplicates
+for index, row in module_df.iterrows():
+    try:
+        cursor.execute("""
+            INSERT INTO ModuleDim (ModuleID, ModuleName, ModuleDescription, ModuleStartDate, ModuleEndDate)
+            VALUES (%s, %s, %s, %s, %s)
+        """, (row['ModuleID'], row['ModuleName'], row['ModuleDescription'], row['ModuleStartDate'], row['ModuleEndDate']))
+    except mysql.connector.IntegrityError as e:
+        print(f"Skipping duplicate entry for ModuleID {row['ModuleID']}")
+        
 # Insert data into StudentDim table with handling duplicates
-for index, row in df.iterrows():
+for index, row in student_df.iterrows():
     try:
         cursor.execute("""
             INSERT INTO StudentDim (StudentID, StudentName, StudentAge, StudentGender, StudentEmail)
