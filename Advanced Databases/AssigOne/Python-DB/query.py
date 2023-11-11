70ffdea97bf022ef36946043d063022e387c48dc
Eskandar Atrakchi <102361045+EskandarAtrakchi@users.noreply.github.com>
2023-11-11 14:59:52 +0000

    .

--- Advanced Databases/AssigOne/Python-DB/query.py
+++ Advanced Databases/AssigOne/Python-DB/query.py
@@ -1,19 +1,17 @@
 import pandas as pd
-import matplotlib.pyplot as plt
-from sqlalchemy import create_engine
 import mysql.connector
-
+from sqlalchemy import create_engine
 
 # Establish a MySQL connection
 conn = mysql.connector.connect(
     host='127.0.0.1',
     user='root',
     password='0723524754Aa.',
-    database='test'
+    database='Assig'
 )
 
 # Create a SQLAlchemy engine using the MySQL connection
-engine = create_engine('mysql+mysqlconnector://root:0723524754Aa.@127.0.0.1/test')
+engine = create_engine('mysql+mysqlconnector://root:0723524754Aa.@127.0.0.1/Assig')
 
 # Execute the SQL query and load the result into a Pandas DataFrame
 query = """
@@ -27,10 +25,14 @@ query = """
 """
 df = pd.read_sql_query(query, engine)
 
+# Close the MySQL connection
+conn.close()
+
 # Plot the data
 plt.figure(figsize=(10, 6))
 plt.bar(df['StudentName'], df['PerformaceScore'], color='skyblue')
 plt.xlabel('Student Name')
 plt.ylabel('Performance Score')
 plt.title('Performance Scores for Students')
+plt.xticks(rotation=45, ha='right')  # there is code in Stackoverflow which shows the rotation for better readablility 
 plt.show()
