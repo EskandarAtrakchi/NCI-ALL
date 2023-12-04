28ecf5de9aa35c67a16536b3767e6e87b52501b8
Eskandar Atrakchi <102361045+EskandarAtrakchi@users.noreply.github.com>
2023-12-04 17:08:40 +0000

    HealthConnectApp

---
+++ OOP/AssigProject3/java NetBeans/HealthConnectApp.java
@@ -0,0 +1,15 @@
+package healthconnect;
+
+/**
+ *
+ * @author eskandar atrakchi
+ */
+public class HealthConnectApp {
+
+    public static void main(String[] args) {
+        
+        HealthConnectUI hc = new HealthConnectUI ();
+        hc.setVisible(true);
+        
+    }
+}
