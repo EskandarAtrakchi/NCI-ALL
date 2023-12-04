c64afdcd6dad77c1fcd9941c9d4f18c4d8101e53
Eskandar Atrakchi <102361045+EskandarAtrakchi@users.noreply.github.com>
2023-12-04 17:11:44 +0000

    UserProfile

---
+++ OOP/AssigProject3/java NetBeans/UserProfile.java
@@ -0,0 +1,77 @@
+
+package healthconnect;
+
+/**
+ *
+ * @author eskandar atrakchi
+ */
+public class UserProfile {
+    
+    protected int patientId;
+    protected String patientFirstName;
+    protected String patientLastName;
+    protected String patientAddress;
+    protected int patientAge;
+    protected String patientPassword;
+
+    public UserProfile() {
+    }
+
+    public UserProfile(int patientId, String patientFirstName, String patientLastName, String patientAddress, int patientAge, String patientPassword) {
+        this.patientId = patientId;
+        this.patientFirstName = patientFirstName;
+        this.patientLastName = patientLastName;
+        this.patientAddress = patientAddress;
+        this.patientAge = patientAge;
+        this.patientPassword = patientPassword;
+    }
+
+    public int getPatientId() {
+        return patientId;
+    }
+
+    public void setPatientId(int patientId) {
+        this.patientId = patientId;
+    }
+
+    public String getPatientFirstName() {
+        return patientFirstName;
+    }
+
+    public void setPatientFirstName(String patientFirstName) {
+        this.patientFirstName = patientFirstName;
+    }
+
+    public String getPatientLastName() {
+        return patientLastName;
+    }
+
+    public void setPatientLastName(String patientLastName) {
+        this.patientLastName = patientLastName;
+    }
+
+    public String getPatientAddress() {
+        return patientAddress;
+    }
+
+    public void setPatientAddress(String patientAddress) {
+        this.patientAddress = patientAddress;
+    }
+
+    public int getPatientAge() {
+        return patientAge;
+    }
+
+    public void setPatientAge(int patientAge) {
+        this.patientAge = patientAge;
+    }
+
+    public String getPatientPassword() {
+        return patientPassword;
+    }
+
+    public void setPatientPassword(String patientPassword) {
+        this.patientPassword = patientPassword;
+    }
+
+}
